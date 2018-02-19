7dc265bffe3c56112d2bc73157e578fab4c6f9f6
kmurban <kcnjhnsn@gmail.com>
2018-02-18 20:02:22 -0800

    Add basic matching logic

--- js/memory-game.js
+++ js/memory-game.js
@@ -1,8 +1,11 @@
-
-
 let cellSize = 0;
+let moves = 0;
+let matches = 0;
+let imageOne = null;
+let imageTwo = null;
 
-//Change the color of the buttons when they are moused over (helps make it clear that these are clickable elements)
+/*Changes the color of the buttons when they are moused over; 
+this helps make it clear that these are clickable elements) */
 
 $('button').mouseover(function(){
 	$(this).css('background-color','#637fa3');
@@ -18,11 +21,12 @@ $('.play-button').click(function(){
 	$('.play-buttons').css('display','none');
 	$('.game-data').css('display','block');
 	$('.reset').css('display','block');
+	$('.move-counter').text('Moves: '+String(moves));
 });
 
-//Helper functions for makeGrid
-//makeRows creates stacked rows (divs) of the correct height
-//fillRows places the correct number of cells (divs) in each row
+/*Helper functions for makeGrid
+makeRows creates stacked rows (divs) of the correct height
+fillRows places the correct number of cells (divs) in each row */
 
 function makeRows(level) {
 	for (i = 0; i < level; i++) {
@@ -43,8 +47,8 @@ function fillRows(level){
 	$('.game-cell').css('height', cellSize);
 }
 
-//Build a grid of the desired size for regular or hard plan
-//Makes use of the helper functions makeRows and fillRows, defined above
+/*Builds a grid of the desired size for regular or hard plan, making 
+use of the helper functions makeRows and fillRows, defined above*/
 
 function makeGrid(level) {
 	if ($(window).width() < $(window).height()) {
@@ -57,6 +61,8 @@ function makeGrid(level) {
 }
 
 
+/*Creates an array of the correct size depending on level
+Populates the array with a whole number for each matchable image*/
 
 function makeImageArray(level) {
 	let images = [];
@@ -69,32 +75,75 @@ function makeImageArray(level) {
 	return images;
 }
 
-function fillImages(level) {
-	let images = makeImageArray(level);
-	let cells = $('.game-cell').get();
-	cells.sort(function(a, b){return 0.5 - Math.random()});
-	console.log(cells);
-	for (i = 0; i < cells.length; i++) {
-		let thisCell = cells[i];
-		let thisImageSrc = "img/"+String(images[i])+".png";
-		$('<img class = "game-image" src = "" alt = "">').appendTo(thisCell);
-		let thisImage = $(thisCell).children().first();
-		$(thisImage).attr('src', thisImageSrc);
-		$(thisImage).attr('alt', thisImageSrc);
-		$(thisImage).css('width',cellSize*0.9);
-	}
+function initImage(image, string) {
+	$(image).attr('src', string);
+	$(image).attr('alt', string);
+	$(image).css('width',cellSize*0.95);
 }
 
-//Initialize the grid when the user selects a play level
+/*Adds images in random order to the game grid
+Note: assumes image files are stored in the "img" folder
+and are in the format #.png*/
+
+function incrementMoves() {
+	moves++;
+	$('.move-counter').text('Moves: ' + String(moves));;
+}
+
+function hideImages(){
+	$(imageOne).css('visibility','hidden');
+	$(imageTwo).css('visibility','hidden');
+}
+
+function resetImages() {
+	imageOne = null;
+	imageTwo = null;
+}
+
+function createImages(level) {
+	let images = makeImageArray(level);
+	let cells = $('.game-cell').get();
+	images.sort(function(a, b){return 0.5 - Math.random()});
+	for (i = 0; i < cells.length; i++) {
+		$('<img class = "game-image" src = "" alt = "">').appendTo(cells[i]);
+		let thisImage = $(cells[i]).children().first();
+		initImage(thisImage, "img/"+String(images[i])+".png");
+	}
+	$('.game-cell').click(function(){
+		if (imageOne == null) {
+			imageOne = $(this).children().first();
+			$(imageOne).css('visibility','visible');
+		} else if (imageTwo == null) {
+			imageTwo = $(this).children().first();
+			$(imageTwo).css('visibility','visible');
+		}
+		if (imageTwo != null && imageOne.attr('src') != imageTwo.attr('src')) {
+			setTimeout(hideImages,500);
+			setTimeout(resetImages,600);
+			setTimeout(incrementMoves,600);
+		} else if (imageTwo != null && imageOne.attr('src') === imageTwo.attr('src')) {
+			matches++;
+			if (matches === (level*level)/2) {
+				alert('You won!');
+			} else {
+				setTimeout(resetImages,100);
+			}
+		}
+	});
+}
+
+//Initializes the grid when the user selects a play level
 
 $('.play-regular').click(function(){
 	makeGrid(4);
-	fillImages(4);
+	createImages(4);
 });
 
 $('.play-hard').click(function(){
 	makeGrid(6);
+	createImages(6);
 });
+
 
 //Clears the grid and brings the Play buttons back up when the user clicks the Start Over buttons
 
